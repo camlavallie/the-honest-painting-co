8533799d2921deec6dd47d8bc595b41aefed773f
Cameron LaVallie <camlavallie@gmail.com>
2020-03-30 14:38:15 -0500

    fixes

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,44 +1,29 @@
 <template>
-<div class="flex flex-wrap lg:justify-between justify-center">
+<div class="flex flex-wrap justify justify-center">
 
   <div class="max-w-sm rounded overflow-hidden shadow-lg m-3">
-  <img class="w-full" src="../assets/images/house5.jpg" alt="Sunset in the mountains">
-  <!-- <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Patterson Lakes</div>
-    <p class="text-gray-700 text-base">
-      Beautiful new paint job on these windows, facia, and the front door, in Patterson Lakes. Want to give your house a facelift? Message us today for a free quote!
-    </p>
-  </div> -->
-  <!-- <div class="px-6 py-4">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-  </div> -->
+  <img src="../assets/images/house5.jpg" alt="Sunset in the mountains">
 </div>
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg m-3">
-  <img class="w-full" src="../assets/images/house2.jpg" alt="Sunset in the mountains">
-  <!-- <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Glen Iris, Victoria</div>
-  </div>
-  <div class="px-6 py-4">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#fresh</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#renewed</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#kitchenpaintjob</span>
-  </div> -->
+  <img src="../assets/images/house2.jpg" alt="Sunset in the mountains">
 </div>
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg m-3">
-  <img class="w-full" src="../assets/images/house4.jpg" alt="Sunset in the mountains">
-  <!-- <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Glen Iris, Victoria</div>
-    <p class="text-gray-700 text-base">
-     Beautiful front deck and entrance! The fretwork was old and faded before painting but look what a lot of prep work and a few coats of paint can do!
-    </p>
-  </div> -->
+  <img src="../assets/images/house4.jpg" alt="Sunset in the mountains">
 </div>
 
+<div class="max-w-sm rounded overflow-hidden shadow-lg m-3">
+  <img src="../assets/images/0-1.jpg" alt="Sunset in the mountains">
+</div>
 
+<div class="max-w-sm rounded overflow-hidden shadow-lg m-3">
+  <img src="../assets/images/reddoor.jpg" alt="Sunset in the mountains">
+</div>
+
+<div class="max-w-sm rounded overflow-hidden shadow-lg m-3">
+  <img src="../assets/images/0-2.jpg" alt="Sunset in the mountains">
+</div>
 
 </div>
 </template>
