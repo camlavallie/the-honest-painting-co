f17a55dc1339fa6d86008c279402abf811354905
Cameron LaVallie <camlavallie@gmail.com>
2020-08-26 13:48:13 -0500

    policy

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,8 @@
     <div class="py-6">
       <h1 class="text-center text-lg text-gray-900 font-bold lg:text-2xl">
         Honest and excellent work.
-        <br><span class="text-red-600">All the time.</span>
+        <br>
+        <span class="text-red-600">All the time.</span>
       </h1>
 
       <hr class="h-px mt-6 bg-gray-700 border-none">
@@ -12,14 +13,26 @@
         <p class="text-center text-gray-500 text-xs mt-3 mb-12">
           &copy;{{new Date().getFullYear()}}
           <span class="font-thin text-xl">The Honest Painting Co</span> All rights reserved.
+          <nuxt-link class="ml-3 hover:text-blue-500" to="/privacy">Privacy Policy</nuxt-link>
+          <nuxt-link class="ml-3 hover:text-blue-500" to="/">Home</nuxt-link>
         </p>
       </div>
-      <div class="flex  pt-1 justify-center">
-  
-      <a href="https://www.instagram.com/thehonestpaintingco/" target="_blank" class="opacity-50 hover:opacity-100 focus:opacity-100"><img class="w-full" src="~/assets/images/instaIcon.svg" alt="" style="max-height: 60px;"></a>
-      <a href="https://www.facebook.com/thehonestpaintingco/" target="_blank" class="opacity-50 hover:opacity-100 focus:opacity-100"><img class="w-full" src="~/assets/images/facebook.svg" alt="" style="max-height: 60px;"></a>
-    
-    </div>
+      <div class="flex pt-1 justify-center">
+        <a
+          href="https://www.instagram.com/thehonestpaintingco/"
+          target="_blank"
+          class="opacity-50 hover:opacity-100 focus:opacity-100"
+        >
+          <img class="w-full" src="~/assets/images/instaIcon.svg" alt style="max-height: 60px;">
+        </a>
+        <a
+          href="https://www.facebook.com/thehonestpaintingco/"
+          target="_blank"
+          class="opacity-50 hover:opacity-100 focus:opacity-100"
+        >
+          <img class="w-full" src="~/assets/images/facebook.svg" alt style="max-height: 60px;">
+        </a>
+      </div>
     </div>
   </footer>
 </template>
