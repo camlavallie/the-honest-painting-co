921862f445ded3837f7298bc22d8407de9ea66d3
Cameron LaVallie <camlavallie@gmail.com>
2020-08-30 21:46:16 -0500

    policy

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -5,9 +5,17 @@
       method="POST"
       class="justify-center w-full"
       id="anchor-name"
+       novalidate="true"
+               @submit="checkForm"
     >
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full px-3">
+                        <p v-if="errors.length">
+    <b class="font-medium">Please correct the following error(s):</b>
+    <ul>
+      <li  v-for="error in errors" class="text-red-500">{{ error }}</li>
+    </ul>
+  </p>
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             for="grid-password"
@@ -15,10 +23,12 @@
             name="name"
           >Name</label>
           <input
-            required
+    
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             type="text"
             name="name"
+              v-model="name"
+              id="name"
           >
         </div>
       </div>
@@ -29,7 +39,8 @@
             for="grid-password"
           >Email</label>
           <input
-            required
+            id="email"
+              v-model="email"
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             type="email"
             name="_replyto"
@@ -40,7 +51,8 @@
         <div class="w-full px-3">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Message</label>
           <textarea
-            required
+              v-model="message"
+              id="message"
             class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
             name="message"
           ></textarea>
@@ -59,7 +71,44 @@
   </div>
 </template>
 
-<script>
-export default {};
-</script>
 
+<script>
+export default {
+  data() {
+    return {
+      errors: [],
+      name: null,
+      message: null,
+      email: null,
+      movie: null
+    };
+  },
+  methods: {
+    checkForm: function(e) {
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push("Name required.");
+      }
+      if (!this.message) {
+        this.errors.push("Message required.");
+      }
+      if (!this.email) {
+        this.errors.push("Email required.");
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push("Valid email required.");
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+
+      e.preventDefault();
+    },
+    validEmail: function(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+};
+</script>
