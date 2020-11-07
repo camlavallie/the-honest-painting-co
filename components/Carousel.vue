fe689fd3f0bd7606cfccc014669e65474e23fc91
Cameron LaVallie <camlavallie@gmail.com>
2020-11-07 08:31:49 -0600

    changes

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -71,7 +71,7 @@
     <div class="max-w-sm rounded overflow-hidden shadow-lg m-3 mb-12">
       <img src="../assets/images/0-1.jpg" alt="Sunset in the mountains">
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Melbourne Painting</div>
+        <div class="font-bold text-xl mb-2">Black Hills Painting</div>
         <p class="text-gray-700 text-base">Painting with excellence.</p>
       </div>
       <div class="px-6 py-4">
@@ -86,7 +86,7 @@
         >Commercial Paint</span>
         <span
           class="inline-block bg-gray-200 rounded-full px-3 py-1 ml-2 text-sm font-semibold text-gray-700"
-        >Melbourne Painters</span>
+        >Black Hills Painters</span>
       </div>
     </div>
 
