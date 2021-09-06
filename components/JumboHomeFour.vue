2a74168000094605346a72a3989dbc4dc530cec2
Cameron LaVallie <camlavallie@gmail.com>
2021-09-06 14:32:45 -0500

    jumbo

--- components/JumboHomeFour.vue
+++ components/JumboHomeFour.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
     <div class="bg-cover bg-center pallet h-auto text-white py-24 px-10 object-fill">
-      <div class="md:w-1/2">
+      <div class="md:w-3/4">
         <p class="font-bold text-sm uppercase">{{this.title1}}</p>
         <p class="text-3xl font-bold">{{this.title2}}</p>
-        <p class="text-2xl mb-10 leading-none">{{this.title3}}</p>
+        <div class="flex flex-wrap items-center">
+
+        <p class="text-2xl mb-10 leading-none flex flex-row items-center ml-3">  <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>Interiors</p>
+        <p class="text-2xl mb-10 leading-none flex flex-row items-center ml-3">  <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>Exteriors</p>
+        <p class="text-2xl mb-10 leading-none flex flex-row items-center ml-3">  <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>Rebuilds</p>
+        <p class="text-2xl mb-10 leading-none flex flex-row items-center ml-3">  <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                            </svg>Repaints</p>
+        </div>
         <!-- <a
           href="#contact"
           class="bg-red-600 py-4 px-8 text-white shadow-md font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800"
@@ -26,7 +40,7 @@ export default {
   data() {
     return {
       title1: "How Can We Help?",
-      title2: "Our Services",
+      title2: "Our Painting Services",
       title3: "All interior and exterior painting.  New builds and repaints."
     };
   },
